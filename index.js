1f3cab986cb7952dc07613e3347dd37318e7fdd3
nozawa <nozawa1821@gmail.com>
2022-07-08 00:57:04 +0900

    params check

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require('dotenv').config()
 const http = require('http')
 
-http
-  .createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.end('Discord bot is active now \n')
-  })
-  .listen(3000)
+http.createServer(function(req, res) {
+  if (req.method == 'POST') {
+    let post_data = ''
+    req.on('data', (chunk) => {
+      post_data += chunk
+    }).on('end', () => {
+      let params = querystring.parse(post_data)
+      // 現在はparamsに対する処理なし
+      if (params.target) console.log(params.target)
+    })
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('active')
+}).listen(3000)
 
 if (process.env.DISCORD_BOT_TOKEN == undefined) {
   console.error('tokenが設定されていません！')
