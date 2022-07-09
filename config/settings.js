e662057c79f6a20d7fdf9053bea7207361a17f56
nozawa <nozawa1821@gmail.com>
2022-07-09 23:07:06 +0900

    poll logic

---
+++ config/settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pnr2: {
+    game_mode_list: [
+      { custom_id: 'game_mode_sg2', name: 'SG', disabled: false },
+      { custom_id: 'game_mode_rt', name: 'RT', disabled: false },
+      { custom_id: 'game_mode_ps', name: 'PS', disabled: false },
+      { custom_id: 'game_mode_ppp', name: 'PPP', disabled: false },
+      { custom_id: 'game_mode_fc', name: 'FC', disabled: false },
+      { custom_id: 'game_mode_bj', name: 'BJ', disabled: false },
+      { custom_id: 'game_mode_ppr', name: 'PPR', disabled: false },
+      { custom_id: 'game_mode_sd', name: 'SD', disabled: false },
+    ]
+  }
+}
